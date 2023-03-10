01190e7417e43901971b0848d077e20be72c05f3
PrusakovAndrey <prusakov.andrey@gmail.com>
2023-03-10 16:52:36 +0300

    second_check

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
-public class Main {
-    
-    public static void main(String[] args) {
-        System.out.print("Hello world!");
+public class Main{
+    public static void main(String[] args){
+        
+        System.out.println("Hello world");
+
     }
+
+
+
 }
 
 
---
+++ Toys.java
@@ -0,0 +1,41 @@
+public class Toys {
+
+    private int id;
+    private String name;
+    private int quantity;
+    private double weight;
+    public Toys(int id, String name, int quantity, double weight) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.weight = weight;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public double getWeight() {
+        return weight;
+    }
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    @Override
+    public String toString() {
+        return "toys [id=" + id + ", name=" + name + ", quantity=" + quantity + ", weight=" + weight + "]";
+    }
+}
